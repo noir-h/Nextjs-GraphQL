20b3862b978d2ab76bf583520c9e6dc8ade83227
noir-h <rose_s98@icloud.com>
2022-03-06 12:36:26 +0900

    add someone

---
+++ project/hooks/useCreateForm.ts
@@ -0,0 +1,55 @@
+import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../queries/queries'
+import { CreateUserMutation } from '../types/generated/graphql'
+
+export const useCreateForm = () => {
+  const [text, setText] = useState('')
+  const [username, setUsername] = useState('')
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
+  const handleTextChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }, [])
+  const usernameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }, [])
+  const printMsg = useCallback(() => {
+    console.log('Hello')
+  }, [])
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      try {
+        await insert_users_one({
+          variables: {
+            name: username,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setUsername('')
+    },
+    // usecallbackでステートを扱う時は初期値で関数オブジェクトが生成されてそれを再利用するため、第二引数に追加する
+    [username]
+  )
+  return {
+    text,
+    handleSubmit,
+    username,
+    usernameChange,
+    printMsg,
+    handleTextChange,
+  }
+}
